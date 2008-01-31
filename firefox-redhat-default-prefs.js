3cc6765ae81c38349519f50f984c63a134d4b1b6
Christopher Aillon <caillon@fedoraproject.org>
2008-01-31 19:47:56 +0000

    Disable the default browser check

--- firefox-redhat-default-prefs.js
+++ firefox-redhat-default-prefs.js
@@ -4,6 +4,7 @@ pref("app.update.autoInstallEnabled", false);
 pref("browser.backspace_action",            2);
 pref("browser.display.use_system_colors",   true);
 pref("browser.link.open_external",          3);
+pref("browser.shell.checkDefaultBrowser",   false);
 pref("general.smoothScroll",                true);
 pref("general.useragent.vendor", "Fedora");
 pref("general.useragent.vendorSub", "FIREFOX_RPM_VR");
