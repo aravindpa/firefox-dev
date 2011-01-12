20da05478a71db7bd0325a054a0cd5a97dc81052
Christopher Aillon <caillon@redhat.com>
2011-01-12 15:56:25 -0800

    Remove global prefs
    Remove global prefs that already exist in our XULRunner package's prefs.

--- firefox-redhat-default-prefs.js
+++ firefox-redhat-default-prefs.js
@@ -6,7 +6,5 @@ pref("browser.display.use_system_colors",   true);
 pref("browser.download.folderList",         1);
 pref("browser.link.open_external",          3);
 pref("browser.shell.checkDefaultBrowser",   false);
-pref("general.smoothScroll",                true);
 pref("general.useragent.vendor", "Fedora");
 pref("general.useragent.vendorSub", "FIREFOX_RPM_VR");
-pref("intl.locale.matchOS", true);
